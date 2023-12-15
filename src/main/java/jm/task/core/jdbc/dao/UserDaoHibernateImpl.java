8f51c79bf10bc252318cb2b4d86902161be3603d
rvilyin <floyslife@yandex.ru>
2023-12-15 05:07:44 +0300

    finished UserDaoHibernateImpl module

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,88 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
     }
 
+    private void createQuery(String sql) {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS User (" +
+                "id BIGINT NOT NULL AUTO_INCREMENT, " +
+                "name VARCHAR(45) NOT NULL, " +
+                "lastName VARCHAR(45) NOT NULL, " +
+                "age TINYINT NOT NULL, " +
+                "PRIMARY KEY (id))";
+        createQuery(sql);
     }
 
     @Override
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS User";
+        createQuery(sql);
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.find(User.class, id);
+            session.remove(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            return session.createQuery("from User", User.class).list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        String sql = "TRUNCATE User";
+        createQuery(sql);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,7 +17,7 @@ public class Util {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "root";
+    private static final String DB_PASSWORD = "rajkiri228";
     private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
@@ -41,7 +41,7 @@ public class Util {
                 settings.put(Environment.URL, DB_URL);
                 settings.put(Environment.USER, DB_USERNAME);
                 settings.put(Environment.PASS, DB_PASSWORD);
-                settings.put(Environment.DIALECT, "MySQL5Dialect");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
                 settings.put(Environment.HBM2DDL_AUTO, "create-drop");
